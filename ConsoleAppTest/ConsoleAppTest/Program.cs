61ee3cc7f9626199cd40b9c9e72aca6bd58102fc
zhongwu <zwu_98@yahoo.com>
2021-01-28 21:33:06 -0800

    add test object

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var test = new Test { name = "test" };
         }
     }
 }
