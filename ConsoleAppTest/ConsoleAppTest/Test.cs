390af8459a26b4dbbd8ab042acea1f52b4f3a894
zhongwu <zwu_98@yahoo.com>
2021-01-28 22:07:16 -0800

    remove address2 and 3

--- ConsoleAppTest/ConsoleAppTest/Test.cs
+++ ConsoleAppTest/ConsoleAppTest/Test.cs
@@ -8,9 +8,7 @@ namespace ConsoleAppTest
     {
         public int id { get; set; }
         public string name { get; set; }
-	    public string address3 {get; set;}
-	    public string address2 {get; set;}
-
+	    public string address {get; set;}
         public string zipcode { get; set; }
     }
 }
