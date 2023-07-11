091d5d2ec20e7f222e0c7e150e519235ca8241ad
Zhong X. Wu <Zhong.X.Wu@kp.org>
2023-07-11 10:04:10 -0700

    remove comment

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            // here is the branch change.
             Console.WriteLine("Hello World!");
             var test = new Test { name = "test" };
             Console.WriteLine(test.name);
