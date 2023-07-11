43dc3312b6400909f14e77f7b0167add14a1f5d9
Zhong X. Wu <Zhong.X.Wu@kp.org>
2023-07-11 09:25:22 -0700

    add comment

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            // this main branch of gittest.
             Console.WriteLine("Hello World!");
             var test = new Test { name = "test" };
             Console.WriteLine(test.name);
