87a5131c156f76892cf1a69bec354646a804b279
Zhong X. Wu <Zhong.X.Wu@kp.org>
2023-07-12 16:45:40 -0700

    add branch comment

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            // here is branch
             Console.WriteLine("Hello World!");
             var test = new Test { name = "test" };
             Console.WriteLine(test.name);
